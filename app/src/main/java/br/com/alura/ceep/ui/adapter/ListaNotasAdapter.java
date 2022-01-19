3be733214234b977df26839a3422b3f3e4eb718d
João Vitor <joaovitormrn@gmail.com>
2022-01-19 18:18:54 -0300

    Removido imports

--- app/src/main/java/br/com/alura/ceep/ui/adapter/ListaNotasAdapter.java
+++ app/src/main/java/br/com/alura/ceep/ui/adapter/ListaNotasAdapter.java
@@ -6,7 +6,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+
 import java.util.List;
+
 import br.com.alura.ceep.R;
 import br.com.alura.ceep.model.Nota;
 
