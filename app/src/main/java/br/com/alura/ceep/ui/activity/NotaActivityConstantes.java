0ff2ec607fbd3a2f5b2877edd1dbf61b5693abd0
João Vitor <joaovitormrn@gmail.com>
2022-01-20 17:35:31 -0300

    Finalizado implementaçãode aplicativo para demonstar o uso de recyclerView para resolver problemas varias views de forma eficiente.

---
+++ app/src/main/java/br/com/alura/ceep/ui/activity/NotaActivityConstantes.java
@@ -0,0 +1,8 @@
+package br.com.alura.ceep.ui.activity;
+
+public interface NotaActivityConstantes {
+
+    public static final String CHAVE_NOTA = "nota";
+    public static final int CODIGO_REQUISICAO_INSERE_NOTA = 1;
+    public static final int CODIGO_RESULTADO_NOTA_CRIADA = 2;
+}
