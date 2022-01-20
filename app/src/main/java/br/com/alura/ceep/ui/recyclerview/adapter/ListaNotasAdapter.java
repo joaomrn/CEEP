9e9acd6df83db994d3ca6d5027bdd7665095db43
João Vitor <joaovitormrn@gmail.com>
2022-01-20 14:00:21 -0300

    Ajuste em classes com erro e remoção de classes não utilizadas

---
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
@@ -0,0 +1,51 @@
+package br.com.alura.ceep.ui.recyclerview.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+import br.com.alura.ceep.R;
+import br.com.alura.ceep.model.Nota;
+
+public class ListaNotasAdapter extends RecyclerView.Adapter {
+
+    private final List<Nota> notas;
+    private final Context context;
+
+    public ListaNotasAdapter(List<Nota> notas, Context context) {
+        this.notas = notas;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false);
+        return new NotaViewHolder(viewCriada);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Nota nota = notas.get(position);
+        TextView titulo = holder.itemView.findViewById(R.id.item_nota_titulo);
+        titulo.setText(nota.getTitulo());
+        TextView descricao = holder.itemView.findViewById(R.id.item_nota_descricao);
+        descricao.setText(nota.getDescricao());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notas.size();
+    }
+
+    class NotaViewHolder extends  RecyclerView.ViewHolder{
+
+        public NotaViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
